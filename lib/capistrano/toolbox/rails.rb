d27ca791a10cd975df75ebb54a151a30c5323075
jijin.huang <ajream.gd@gmail.com>
2017-01-13 17:49:13 +0800

    init this gem

---
+++ lib/capistrano/toolbox/rails.rb
@@ -0,0 +1,9 @@
+require "capistrano/toolbox/rails/version"
+
+module Capistrano
+  module Toolbox
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/toolbox/rails/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Toolbox
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
